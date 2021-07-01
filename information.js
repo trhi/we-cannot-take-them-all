c601696c60c69755ba457964ced4601c0bf61506
trhi <terhim@gmail.com>
2021-07-01 14:37:39 +0100

    Update filenames and artist statement in comments

---
+++ information.js
@@ -0,0 +1,19 @@
+var INFO = {
+"rmation":
+["The reasons that you hear are based on an imaginary list of reasons " +
+"for why Americans were against Jewish refugees during World War Two. ",
+"After the war, the full extent of the holocaust was revealed. " ,
+"Quote: 'If human life and security is at stake, countries should "+
+"be morally obliged to permit the entry of non-nationals.' " ,
+"This is the moral groundwork of the United Nations 1951 Refugee Convention." ,
+"The statements used here are based on a text on page 193 of a book by " +
+"Joseph Carens called The Ethics of Immigration (2015)."
+]
+};
+
+/*
+
+
+
+
+*/
